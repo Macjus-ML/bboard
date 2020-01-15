9aa1fd3d414bc35bd484be9b64460ab01da12c75
Macjus-ML <abolshakovy@gmail.com>
2020-01-15 22:08:10 +0300

    Add Authorization

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from  .views import index
+from .views import index, other_page, BBLoginView, BBLogoutView
+from .views import BBPasswordChangeView
+from .views import profile, ChangeUserInfoView
 
 app_name = 'main'
 urlpatterns = [
-
+    path('accounts/password/change', BBPasswordChangeView.as_view(),name='password_change'),
+    path('accounts/profile/change', ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+    path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
 
 ]
